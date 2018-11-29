88d3f36a7618b32855eb3c8bfa164aaa4772fa7d
Julio Lopez <julioj.lopeza@gmail.com>
2018-11-29 01:08:07 -0300

    closures

---
+++ JS ES6/closureExample.js
@@ -0,0 +1,14 @@
+function makeHelloFunction() {
+    const message = 'Hello!'
+
+    function sayHello() {
+        console.log(message)
+    }
+    
+    return sayHello
+}
+const sayHello = makeHelloFunction()
+
+console.log(message)
+
+sayHello()
---
+++ JS ES6/clousureBug.js
@@ -0,0 +1,11 @@
+function makeFunctionArray() {
+    const arr = []
+
+    for (var i = 0; i < 5 ; i++) {
+        arr.push(function(){ console.log(i)})
+    }
+    return arr
+}
+const functionArr = makeFunctionArray()
+
+functionArr[0]()
